3e77defed2d42febd2f959885e79e8f13f4eed9f
aannaasttasia <nastyahavruliyk@gmail.com>
2023-02-21 13:34:39 +0200

    last commit

--- button.js
+++
@@ -1,5 +0,0 @@
-var tag = document.createElement("p");
-var text = document.createTextNode("Tutorix is the best e-learning platform");
-tag.appendChild(text);
-var element = document.getElementById("new");
-element.appendChild(tag);
